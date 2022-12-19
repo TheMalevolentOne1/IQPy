a5d547c2ebd6660e40ab20b271b65a4b3e5933ad
TheMalevolentOne1 <kylerobinson818@gmail.com>
2022-12-19 18:47:43 +0000

    Create PythonAkos.py

---
+++ PythonAkos.py
@@ -0,0 +1,37 @@
+'''
+https://mradamscs.files.wordpress.com/2018/06/python_complete.pdf -- link to challenges
+
+these are the fundamental basics of programming how are you finding them?
+
+any difficult areas?
+
+
+strings are annoying LMAO
+'''
+
+'''
+# Challenge 1
+
+age = int(input("age?: "))
+print(f"your age is {age}")
+
+# Lesson 2
+
+'''
+'''
+Notes:
+
++ means add
+- means subtract
+* means times
+/ means divide 
+% means modular (dont worry about this piece of shit yet)
+
+'''
+
+x = 1  # Change these numbers
+
+x = x * 2  # using the arithmetic cheat sheet above times it by the number you set
+
+print(x)
+
